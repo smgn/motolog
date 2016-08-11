a9feae9f17f55483fd402b7358f648798371b42d
Terence Yeong <terenceyeong@gmail.com>
2016-08-12 02:15:54 +0800

    Rework refuel widget code, but consumption calculation is always 0

--- app/src/main/java/com/kaetter/motorcyclemaintenancelog/RefuelActivity.java
+++ app/src/main/java/com/kaetter/motorcyclemaintenancelog/RefuelActivity.java
@@ -1,13 +1,16 @@
 package com.kaetter.motorcyclemaintenancelog;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.Cursor;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.text.TextUtilsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
@@ -18,329 +21,114 @@ import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.logging.Logger;
 
 import beans.MaintenanceItem;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import dbcontrollers.MainHelper;
 import dbcontrollers.MainLogSource;
 
-public class RefuelActivity extends FragmentActivity {
+public class RefuelActivity extends AppCompatActivity {
+
+    @BindView(R.id.tilFuel) TextInputLayout tilFuel;
+    @BindView(R.id.tilOdometer) TextInputLayout tilOdometer;
+    @BindView(R.id.tilPrice) TextInputLayout tilPrice;
+    @BindView(R.id.editTextFuel) EditText editTextFuel;
+    @BindView(R.id.editTextOdometer) EditText editTextOdometer;
+    @BindView(R.id.editTextPrice) EditText editTextPrice;
 
 	private MainLogSource mainLogSource;
-	private double fuelAmount, cash, cashPerLitre;
-	private int odometer;
 	private int mileageType;
-	private SharedPreferences sharedPrefs;
-	private String username, pass;
+    private int cashType;
 
-	@Override
+    @Override
 	protected void onCreate(Bundle arg0) {
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(arg0);
 
 		setContentView(R.layout.refuelactivity);
-		final EditText fuelAmountView = (EditText) findViewById(R.id.editTextFuel);
-		final EditText odoTextView = (EditText) findViewById(R.id.odoText);
-		final EditText cashTextView = (EditText) findViewById(R.id.editTextPrice);
 
-		sharedPrefs = PreferenceManager
-				.getDefaultSharedPreferences(getApplicationContext());
-		mileageType = Integer.parseInt(sharedPrefs.getString(
-				"pref_MileageType", "0"));
+        ButterKnife.bind(this);
+
+        SharedPreferences sharedPrefs =
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		mileageType = Integer.parseInt(sharedPrefs.getString("pref_MileageType", "0"));
+        cashType = Integer.parseInt(sharedPrefs.getString("pref_CashType", "0"));
 
 		if (mileageType == 1) {
-			TextView litres = (TextView) findViewById(R.id.litres);
-			litres.setText("gallons");
-			TextView odoLabel = (TextView) findViewById(R.id.odoLabel);
-			odoLabel.setText("miles");
-
-		}
-
-		if (!sharedPrefs.getString("email", "").equals("")) {
-
-			findViewById(R.id.notification).setVisibility(View.VISIBLE);
+            tilFuel.setHint(getString(R.string.activity_refuel_hint_fuel_amount_gallons));
+            tilOdometer.setHint(
+                    getString(R.string.activity_refuel_hint_odometer_reading_gallons));
+            tilPrice.setHint(getString(R.string.activity_refuel_hint_price_gallon));
 		} else {
-			findViewById(R.id.notification).setVisibility(View.INVISIBLE);
+            tilFuel.setHint(getString(R.string.activity_refuel_hint_fuel_amount_litres));
+            tilOdometer.setHint(getString(R.string.activity_refuel_hint_odometer_reading_km));
+            tilPrice.setHint(getString(R.string.activity_refuel_hint_price_litre));
 		}
 
-		final int cashType = Integer.parseInt(sharedPrefs.getString(
-				"pref_CashType", "0"));
+		mainLogSource = new MainLogSource(getApplication().getApplicationContext());
 
-		String unit = (mileageType == 1) ? "gallon" : "litre";
+        editTextFuel.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
-		TextView cashTypeTextView = (TextView) findViewById(R.id.cashType);
-		String cashTypeText = (cashType == 0) ? "price/" + unit
-				: "price per fill-up!";
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() == 0) {
+                    tilFuel.setError(getString(R.string.error_required));
+                } else {
+                    tilFuel.setErrorEnabled(false);
+                }
+            }
 
-		cashTypeTextView.setText(cashTypeText);
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
 
-		mainLogSource = new MainLogSource(getApplication()
-				.getApplicationContext());
+        editTextOdometer.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
-		odoTextView.setOnEditorActionListener(new OnEditorActionListener() {
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() == 0) {
+                    tilOdometer.setError(getString(R.string.error_required));
+                } else {
+                    tilOdometer.setErrorEnabled(false);
+                }
+            }
 
-			@Override
-			public boolean onEditorAction(TextView v, int actionId,
-					KeyEvent event) {
-
-				if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER))
-						|| (actionId == EditorInfo.IME_ACTION_DONE)) {
-
-					Date date = new Date();
-
-					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-
-					String today = sdf.format(date);
-
-					String vehicle = "default";
-					String[] maintElemArray = getResources().getStringArray(
-							R.array.maintElemArray);
-					String maintElem = maintElemArray[0];
-					String[] maintTypeArray = getResources().getStringArray(
-							R.array.maintTypeArray);
-					String maintType = maintTypeArray[0];
-
-					if (!fuelAmountView.getText().toString().equals("")) {
-
-						fuelAmount = Double.parseDouble(fuelAmountView
-								.getText().toString());
-					} else {
-						Toast.makeText(getApplicationContext(),
-								"Fuel amount must be entered!",
-								Toast.LENGTH_LONG).show();
-						return false;
-					}
-
-					if (!odoTextView.getText().toString().equals("")) {
-						odometer = Integer.parseInt(odoTextView.getText()
-								.toString());
-
-					} else {
-						Toast.makeText(getApplicationContext(),
-								"ODO must be entered!", Toast.LENGTH_LONG)
-								.show();
-						return false;
-					}
-
-					if (cashTextView.getText().toString().equals("")) {
-						cash = 0;
-						cashPerLitre = 0;
-					}
-
-					try {
-						cash = Double.parseDouble(cashTextView.getText()
-								.toString());
-					} catch (NumberFormatException e) {
-
-						Toast.makeText(getApplicationContext(),
-								"Invalid number for price!", Toast.LENGTH_LONG)
-								.show();
-						return false;
-					}
-
-					if (cashType == 0) {
-						cashPerLitre = cash;
-						cash = cash * fuelAmount;
-					} else {
-						cashPerLitre = cash / fuelAmount;
-					}
-					cashPerLitre = Math.round(cashPerLitre * 100.0) / 100.0;
-					cash = Math.round(cash * 100.0) / 100.0;
-					double consumption = getConsumption(vehicle, maintElem,
-							fuelAmount, odometer);
-
-					String details = "quick entry";
-
-					MaintenanceItem mItem = new MaintenanceItem(vehicle,
-							maintElem, maintType, fuelAmount, consumption,
-							today, odometer, details, mileageType, cash);
-
-					mainLogSource.addMaintenanceItem(mItem);
-
-					if (mileageType == 0) {
-
-						Toast.makeText(
-								getApplicationContext(),
-								"New fuel entry added!\r\n\r\nFuel consumption is :"
-										+ consumption
-										+ "l/100km\r\n\r\nHave a safe trip!",
-								Toast.LENGTH_LONG).show();
-						finish();
-
-					} else {
-						if (mileageType == 2) {
-
-							Toast.makeText(
-									getApplicationContext(),
-									"New fuel entry added!\r\n\r\nFuel consumption is :"
-											+ consumption
-											+ "l/km\r\n\r\nHave a safe trip!",
-									Toast.LENGTH_LONG).show();
-							finish();
-						} else {
-							Toast.makeText(
-									getApplicationContext(),
-									"New fuel entry added!\r\n\r\nFuel consumption is :"
-											+ consumption
-											+ "MPG\r\n\r\nHave a safe trip!",
-									Toast.LENGTH_LONG).show();
-							finish();
-						}
-
-					}
-				}
-				return false;
-			}
-		});
-		cashTextView.setOnEditorActionListener(new OnEditorActionListener() {
-
-			@Override
-			public boolean onEditorAction(TextView v, int actionId,
-					KeyEvent event) {
-
-				if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER))
-						|| (actionId == EditorInfo.IME_ACTION_DONE)) {
-
-					Date date = new Date();
-
-					SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-
-					String today = sdf.format(date);
-
-					String vehicle = "default";
-					String[] maintElemArray = getResources().getStringArray(
-							R.array.maintElemArray);
-					String maintElem = maintElemArray[0];
-					String[] maintTypeArray = getResources().getStringArray(
-							R.array.maintTypeArray);
-					String maintType = maintTypeArray[0];
-
-					if (!fuelAmountView.getText().toString().equals("")) {
-
-						fuelAmount = Double.parseDouble(fuelAmountView
-								.getText().toString());
-					} else {
-						Toast.makeText(getApplicationContext(),
-								"Fuel amount must be entered!",
-								Toast.LENGTH_LONG).show();
-						return false;
-					}
-
-					if (!odoTextView.getText().toString().equals("")) {
-						odometer = Integer.parseInt(odoTextView.getText()
-								.toString());
-
-					} else {
-						Toast.makeText(getApplicationContext(),
-								"ODO must be entered!", Toast.LENGTH_LONG)
-								.show();
-						return false;
-					}
-					if (cashTextView.getText().toString().equals("")) {
-						cash = 0;
-						cashPerLitre = 0;
-					} else {
-						try {
-							cash = Double.parseDouble(cashTextView.getText()
-									.toString());
-						} catch (NumberFormatException e) {
-
-							Toast.makeText(getApplicationContext(),
-									"Invalid number for price!",
-									Toast.LENGTH_LONG).show();
-							return false;
-						}
-					}
-
-					if (cashType == 0) {
-						cashPerLitre = cash;
-						cash = cash * fuelAmount;
-
-					} else {
-						cashPerLitre = cash / fuelAmount;
-					}
-					cashPerLitre = Math.round(cashPerLitre * 100.0) / 100.0;
-					cash = Math.round(cash * 100.0) / 100.0;
-
-					// if value is correct then cash
-
-					double consumption = getConsumption(vehicle, maintElem,
-							fuelAmount, odometer);
-
-					String details = "quick entry";
-
-					MaintenanceItem mItem = new MaintenanceItem(vehicle,
-							maintElem, maintType, fuelAmount, consumption,
-							today, odometer, details, mileageType, cash);
-
-					mainLogSource.addMaintenanceItem(mItem);
-
-					if (mileageType == 0) {
-
-						Toast.makeText(
-								getApplicationContext(),
-								"New fuel entry added!\r\n\r\nFuel consumption is :"
-										+ consumption
-										+ "l/100km\r\n\r\nHave a safe trip!",
-								Toast.LENGTH_LONG).show();
-						finish();
-
-					} else {
-						if (mileageType == 2) {
-
-							Toast.makeText(
-									getApplicationContext(),
-									"New fuel entry added!\r\n\r\nFuel consumption is :"
-											+ consumption
-											+ "l/km\r\n\r\nHave a safe trip!",
-									Toast.LENGTH_LONG).show();
-							finish();
-						} else {
-							Toast.makeText(
-									getApplicationContext(),
-									"New fuel entry added!\r\n\r\nFuel consumption is :"
-											+ consumption
-											+ "MPG\r\n\r\nHave a safe trip!",
-									Toast.LENGTH_LONG).show();
-							finish();
-						}
-
-					}
-				}
-				return false;
-			}
-		});
-
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
 	}
 
-	public double getConsumption(String vehicle, String maintElem, double fuel,
-			int odometer) {
-
+	public double getConsumption(String vehicle, String maintElem, double fuel, int odometer) {
 		Cursor cursor = mainLogSource.getLastItem(vehicle, maintElem);
 
+        //TODO: cursor is always null right now, why?
 		if (cursor != null) {
-
 			double consumption;
-			if (mileageType == 0) {
-				consumption = 100
-						* fuel
-						/ (odometer - cursor.getDouble(cursor
-								.getColumnIndex(MainHelper.FIELD7)));
-
-			} else {
-				if (mileageType == 2) {
-					consumption = fuel
-							/ (odometer - cursor.getDouble(cursor
-									.getColumnIndex(MainHelper.FIELD7)));
-
-				} else {
-
-					consumption = (odometer - cursor.getDouble(cursor
-							.getColumnIndex(MainHelper.FIELD7))) / fuel;
-				}
-
-			}
+            switch (mileageType) {
+                case 0:
+                    consumption = 100
+                            * fuel
+                            / (odometer - cursor.getDouble(cursor
+                            .getColumnIndex(MainHelper.FIELD7)));
+                    break;
+                case 2:
+                    consumption = fuel
+                            / (odometer - cursor.getDouble(cursor
+                            .getColumnIndex(MainHelper.FIELD7)));
+                    break;
+                default:
+                    consumption = (odometer - cursor.getDouble(cursor
+                            .getColumnIndex(MainHelper.FIELD7))) / fuel;
+                    break;
+            }
 
 			consumption = Math.round(consumption * 100.0) / 100.0;
 
@@ -349,17 +137,104 @@ public class RefuelActivity extends FragmentActivity {
 			} else {
 				return consumption;
 			}
-
 		} else
-
 			return 0;
-
 	}
+
+    @OnClick(R.id.buttonCreate)
+    public void buttonCreateOnClick() {
+
+        double fuelAmount;
+        int odometer;
+        double cash;
+
+        String today = new SimpleDateFormat("yyyy/MM/dd", Locale.US).format(new Date());
+
+        String vehicle = "default";
+        String[] maintElemArray = getResources().getStringArray(R.array.maintElemArray);
+        String maintElem = maintElemArray[0];
+        String[] maintTypeArray = getResources().getStringArray(R.array.maintTypeArray);
+        String maintType = maintTypeArray[0];
+
+        // validation
+        if (!TextUtils.isEmpty(editTextFuel.getText().toString())) {
+            fuelAmount = Double.parseDouble(editTextFuel.getText().toString());
+        } else {
+            tilFuel.setError(getString(R.string.error_required));
+            return;
+        }
+
+        if (!TextUtils.isEmpty(editTextOdometer.getText().toString())) {
+            odometer = Integer.parseInt(editTextOdometer.getText().toString());
+        } else {
+            tilOdometer.setError(getString(R.string.error_required));
+            return;
+        }
+        if (TextUtils.isEmpty(editTextPrice.getText().toString())) {
+            cash = 0;
+        } else {
+            try {
+                cash = Double.parseDouble(editTextPrice.getText()
+                        .toString());
+            } catch (NumberFormatException e) {
+                Toast.makeText(this, getString(R.string.error_price), Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
+        // end validation
+
+        if (cashType == 0) {
+            cash = cash * fuelAmount;
+        }
+        cash = Math.round(cash * 100.0) / 100.0;
+
+        double consumption = getConsumption(vehicle, maintElem, fuelAmount, odometer);
+
+        String details = "quick entry";
+
+        MaintenanceItem mItem = new MaintenanceItem(vehicle,
+                maintElem, maintType, fuelAmount, consumption,
+                today, odometer, details, mileageType, cash);
+
+        mainLogSource.addMaintenanceItem(mItem);
+
+        switch (mileageType) {
+            case 0:
+                Toast.makeText(this,
+                        getString(R.string.text_new_fuel_entry_added) +
+                                "\r\n\r\n" + getString(R.string.text_fuel_consumption_is) +
+                                consumption +
+                                "l/100km" +
+                                "\r\n\r\n" +
+                                getString(R.string.text_have_a_safe_trip),
+                        Toast.LENGTH_LONG).show();
+                finish();
+            case 2:
+                Toast.makeText(this,
+                        getString(R.string.text_new_fuel_entry_added) +
+                                "\r\n\r\n" + getString(R.string.text_fuel_consumption_is) +
+                                consumption +
+                                "l/km" +
+                                "\r\n\r\n" +
+                                getString(R.string.text_have_a_safe_trip),
+                        Toast.LENGTH_LONG).show();
+                finish();
+            default:
+                Toast.makeText(this,
+                        getString(R.string.text_new_fuel_entry_added) +
+                                "\r\n\r\n" + getString(R.string.text_fuel_consumption_is) +
+                                consumption +
+                                "MPG" +
+                                "\r\n\r\n" +
+                                getString(R.string.text_have_a_safe_trip),
+                        Toast.LENGTH_LONG).show();
+                finish();
+        }
+    }
 
 	@Override
 	protected void onPause() {
 		super.onPause();
 		finish();
 	}
-
 }
--- app/src/main/java/com/kaetter/motorcyclemaintenancelog/RefuelWidgetProvider.java
+++ app/src/main/java/com/kaetter/motorcyclemaintenancelog/RefuelWidgetProvider.java
@@ -10,47 +10,22 @@ import android.widget.RemoteViews;
 public class RefuelWidgetProvider extends AppWidgetProvider {
 
 	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-			int[] appWidgetIds) {
-	
-		
-		for (int i = 0; i < appWidgetIds.length; i++) {
-			
-		 AppWidgetManager mgr = AppWidgetManager.getInstance(context);
-		 
-		 int appWidgetId = appWidgetIds[i];
-		 
-		 Intent intent = new Intent(context,RefuelActivity.class); 
-		 
-		 intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            AppWidgetManager mgr = AppWidgetManager.getInstance(context);
 
-		 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		 PendingIntent myPI = PendingIntent.getActivity(context, 0, intent, 0); 
+            Intent intent = new Intent(context, RefuelActivity.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-		 RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.refuelwidget);
-		 views.setOnClickPendingIntent(R.id.refuel_linear_layout, myPI); 
-		 
-		 mgr.updateAppWidget(appWidgetId, views); 
-		 
-		}
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-		
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.refuelwidget);
+            views.setOnClickPendingIntent(R.id.refuel_linear_layout, pendingIntent);
+
+            mgr.updateAppWidget(appWidgetId, views);
+        }
 	}
 
-	@Override
-	public void onEnabled(Context context) {
-
-		    
-		super.onEnabled(context);
-		
-		
-		
-		
-		
-		
-		
-	}
-	
-	
 
 }
